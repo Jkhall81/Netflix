3900871bd591444982f2f81eda70e2dfbb988a78
Jkhall81 <jason.kei.hall@gmail.com>
2022-07-18 16:59:41 -0700

    currently working on row, commiting to a branch

---
+++ src/Requests.js
@@ -0,0 +1,11 @@
+const key = 'fd6fddaa4a5729b92055a1ffb64627fb'
+
+const requests = {
+    requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
+    requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=1`,
+    requestTrending: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=2`,
+    requestHorror: `https://api.themoviedb.org/3/search/movie?api_key=${key}&language=en-US&query=horror&page=1&include_adult=false`,
+    requestUpcoming: `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=1`,
+  };
+
+  export default requests
