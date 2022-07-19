491fcfd6fcb07c8469e3922740c2a177a7930d00
Jkhall81 <jason.kei.hall@gmail.com>
2022-07-19 16:55:18 -0700

    added authentication

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
 import { Route, Routes } from "react-router";
 import Navbar from "./components/Navbar";
-import Home from './pages/Home';
-
+import Home from "./pages/Home";
+import { AuthContextProvider } from "./context/AuthContext";
+import Login from "./pages/Login";
+import Account from "./pages/Account";
+import Signup from "./pages/Signup";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   return (
     <>
-      <Navbar />
-      <Routes>
-        <Route path='/' element={<Home />} />
-      </Routes>
+      <AuthContextProvider>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route
+            path="/account"
+            element={
+              <ProtectedRoute>
+                <Account />
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      </AuthContextProvider>
     </>
   );
 }
